edf72096b79b44d3ac6f539744d4f7e781b8e7f9
minhliem03 <93208245+tnminhliem@users.noreply.github.com>
2024-03-26 21:24:43 +0700

    update-1

---
+++ ocourse/courses/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django import forms
+from django.utils.html import mark_safe
+from courses.models import Category, Course, Lesson, User, Tag
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+class CourseForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class MyCourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_date', 'updated_date', 'active']
+    search_fields = ['name', 'description']
+    list_filter = ['id', 'name', 'created_date']
+    readonly_fields = ['my_image']
+    form = CourseForm
+
+    def my_image(self, instance):
+        if instance:
+            return mark_safe(f"<img width='120' src ='/static/{instance.image.name}'/>")
+
+    class Media:
+        css = {
+            'all': ('/static/css/style.css', )
+        }
+
+
+admin.site.register(Category)
+admin.site.register(Course, MyCourseAdmin)
+admin.site.register(Lesson)
+admin.site.register(User)
+admin.site.register(Tag)
+
---
+++ ocourse/courses/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime
+from ckeditor.fields import RichTextField
+from cloudinary.models import CloudinaryField
+
+class User (AbstractUser):
+    avatar = CloudinaryField(null = True)
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class ItemBase(BaseModel):
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        abstract = True
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Course(ItemBase):
+    name = models.CharField(max_length=255)
+    description = RichTextField()
+    image = CloudinaryField(null = True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Lesson(ItemBase):
+    subject = models.CharField(max_length=255)
+    content = RichTextField()
+    image = CloudinaryField(null = True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Interaction(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+class Comment(Interaction):
+    content = models.CharField(max_length=255)
+
+class Like(Interaction):
+    class Meta:
+        unique_together = ('user', 'lesson')
